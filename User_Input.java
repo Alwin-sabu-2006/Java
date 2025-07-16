e423ff01fd0565aeaf8f9fad38204c0779eb931d
Alwin Sabu <alwinsabu2028@cs.sjcetpalai.ac.in>
2025-07-16 10:20:12 +0530

    Add files via upload

---
+++ User_Input.java
@@ -0,0 +1,12 @@
+package first_java;
+import java.util.Scanner;
+public class User_Input {
+  public static void main(String[]args)
+  {
+	  Scanner sc=new Scanner(System.in);
+	  System.out.println("Enter the number:");
+	  int number=sc.nextInt();
+	  System.out .println("The new number is:"+number);
+	  
+  }
+}
