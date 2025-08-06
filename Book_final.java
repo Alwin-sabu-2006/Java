325aa620d0e16f6fc7446e2b2b53871ea3cb2bc3
Alwin Sabu <alwinsabu2028@cs.sjcetpalai.ac.in>
2025-08-06 10:49:27 +0530

    Create Book_final.java

---
+++ Book_final.java
@@ -0,0 +1,24 @@
+package javaprgm;
+class Book
+{
+	String title;
+	String author;
+	final int book_id=111;
+	final static String Library_name="Sjcet";
+	static int book_counter=1000;
+	
+	Book()
+	{
+		title="No title";
+		author="No Author";
+	}
+	Book(String title,String author){
+		this.title=title;
+		this.author=author;
+	}
+	public void displayInfo(String author)
+}
+
+public class Book_Final {
+
+}
